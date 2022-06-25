33fdbdd4f4494671d442e948e6a4b687731c69ae
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-06-25 16:52:58 -0400

    removed unreachable return

--- apps/nested-routes-remix/src/routes/__authenticated/settings.tsx
+++ apps/nested-routes-remix/src/routes/__authenticated/settings.tsx
@@ -10,13 +10,4 @@ export default function SettingsView() {
       </Link>
     </VerticalContainer>
   );
-
-  return (
-    <VerticalContainer className="p-4">
-      <h2>Settings</h2>
-      <Link className="link mt-4" to="/accounts">
-        Back to Accounts
-      </Link>
-    </VerticalContainer>
-  );
 }
